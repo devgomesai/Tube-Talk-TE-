28e1a0c73426442066d9bd08e4699df983ec163d
Vivian Ludrick <116781909+vivalchemy@users.noreply.github.com>
2024-10-28 17:54:11 +0530

    chore: auth ui done

---
+++ web/app/auth/page.tsx
@@ -0,0 +1,12 @@
+import { SignupFormDemo } from '@/components/blocks/SignUp'
+import React from 'react'
+
+function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8">
+      <SignupFormDemo />
+    </div>
+  )
+}
+
+export default Page
